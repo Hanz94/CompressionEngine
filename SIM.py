4f8bc3f4c81aafbd409907386f1b923622f0d39d
Hansika <hansikam94@gmail.com>
2021-03-30 01:59:03 -0400

    initializing dictionary

--- SIM.py
+++ SIM.py
@@ -2,6 +2,10 @@
 # “I have neither given nor received any unauthorized aid on this assignment”.
 # UFID -: 1163-9514
 # Hansika Weerasena
+import collections
+import operator
+
+dictionary = []
 
 def readFileLineByLine(filename):
     file1 = open(filename, 'r')
@@ -12,5 +16,17 @@ def readFileLineByLine(filename):
     file1.close()
     return itemList
 
-filez = open("simulation.txt", 'w')
+def createDictionary(elements):
+    counter = collections.Counter(elements)
+    dictionary = dict(sorted(counter.items(), key=operator.itemgetter(1), reverse=True))
+    return list(dictionary.keys())[:16]
+
+originalCode = readFileLineByLine("original.txt")
+dictionary = createDictionary(originalCode)
+
+codeEntryIndex = 0
+while codeEntryIndex < len(originalCode):
+  print(originalCode[codeEntryIndex])
+  codeEntryIndex = codeEntryIndex + 1
+
 
