07e76491da22a0c737785337ae5b4197fbb1a138
Hansika <hansikam94@gmail.com>
2021-03-30 12:40:32 -0400

    implemented compression

--- SIM.py
+++ SIM.py
@@ -40,16 +40,17 @@ def getXORedString(entry, dicEntry):
     y = int(entry, 2) ^ int(dicEntry, 2)
     return '{0:b}'.format(y)
 
-def getXORedString(entry, dicEntry):
-    y = int(entry, 2) ^ int(dicEntry, 2)
-    return '{0:b}'.format(y)
+def getXORedChunk(str):
+    x = str.rfind('1')
+    y = str.find('1')
+    return str[y:x+1]
 
 originalCode = readFileLineByLine("original.txt")
 dictionary = createDictionary(originalCode)
 
 codeEntryIndex = 0
 previousCodeEntry = ''
-compressedCode = ''
+compressedCode = []
 
 while codeEntryIndex < len(originalCode):
     increment = 1
@@ -63,14 +64,49 @@ while codeEntryIndex < len(originalCode):
             compressedEntry = '111' + bin(exactMatchIndex)[2:].zfill(4)
         else:
             isMatched = False
-            for dicEntry in dictionary:
-                xorString = getXORedString(originalCode[codeEntryIndex])
+            for i in range(len(dictionary)):
+                xorString = getXORedString(originalCode[codeEntryIndex], dictionary[i])
                 length = len(xorString)
-                xorChunk =
-
-
-
-
-    compressedCode = compressedCode + compressedEntry
+                xorChunk = getXORedChunk(xorString)
+                if int(xorChunk, 2) == 1:
+                    compressedEntry = '011' + bin(32 - length).zfill(5) + bin(i)[2:].zfill(4)
+                    isMatched = True
+                    break
+                elif int(xorChunk, 2) == 3:
+                    compressedEntry = '100' + bin(32 - length).zfill(5) + bin(i)[2:].zfill(4)
+                    isMatched = True
+                    break
+                elif int(xorChunk, 2) == 15:
+                    compressedEntry = '101' + bin(32 - length).zfill(5) + bin(i)[2:].zfill(4)
+                    isMatched = True
+                    break
+                else:
+                    continue
+            if not isMatched:
+                for i in range(len(dictionary)):
+                    xorString = getXORedString(originalCode[codeEntryIndex], dictionary[i])
+                    length = len(xorString)
+                    xorChunk = getXORedChunk(xorString)
+                    if len(xorChunk) < 5:
+                        xorChunk = xorChunk.ljust(4, '0')
+                        compressedEntry = '010' + bin(32 - length).zfill(5) + xorChunk + bin(i)[2:].zfill(4)
+                        isMatched = True
+                        break
+                if not isMatched:
+                    for i in range(len(dictionary)):
+                        xorString = getXORedString(originalCode[codeEntryIndex], dictionary[i])
+                        length = len(xorString)
+                        xorChunk = getXORedChunk(xorString)
+                        if xorChunk.count('11')*2 == xorChunk('1') == 4:
+                            firstIndex = 32 - length
+                            secondIndex = firstIndex + xorChunk.rfind('11')
+                            compressedEntry = '010' + bin(firstIndex).zfill(5) + bin(secondIndex).zfill(5) + bin(i)[2:].zfill(4)
+                            isMatched = True
+                            break
+                    if not isMatched:
+                        compressedEntry = '000' + originalCode[codeEntryIndex]
+    compressedCode.append(compressedEntry)
     previousCodeEntry = originalCode[codeEntryIndex]
     codeEntryIndex = codeEntryIndex + increment
+
+print(compressedCode)
