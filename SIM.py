ad095888cbf01335d7b87f27478412630ba7779d
Hansika <hansikam94@gmail.com>
2021-03-30 13:53:08 -0400

    bug fixes in compression

--- SIM.py
+++ SIM.py
@@ -1,11 +1,11 @@
 # “I have neither given nor received any unauthorized aid on this assignment”.
 # UFID -: 1163-9514
 # Hansika Weerasena
+
 import collections
 import operator
 
 dictionary = []
-
 
 def readFileLineByLine(filename):
     file1 = open(filename, 'r')
@@ -25,10 +25,11 @@ def createDictionary(elements):
 
 def RLE(entry, index, originalCode):
     count = 0
-    while entry == originalCode[index] and count < 9:
+    while entry == originalCode[index] and count < 8:
         count = count + 1
         index = index + 1
     return count
+
 
 def exactMatch(codeEnty):
     try:
@@ -36,31 +37,55 @@ def exactMatch(codeEnty):
     except ValueError:
         return -1
 
+
 def getXORedString(entry, dicEntry):
     y = int(entry, 2) ^ int(dicEntry, 2)
     return '{0:b}'.format(y)
+
 
 def getXORedChunk(str):
     x = str.rfind('1')
     y = str.find('1')
     return str[y:x+1]
 
+
+def chunkstring(string, length):
+    return list(string[0+i:length+i] for i in range(0, len(string), length))
+
+
+def writeToFile(compressedCode):
+    open('somefile.txt', 'w').close()
+    compressedEntries = chunkstring(compressedCode, 32)
+    with open('somefile.txt', 'a') as the_file:
+        for i in range(len(compressedEntries)):
+            the_file.write(compressedEntries[i].ljust(32, '0') + "\n")
+        the_file.write('xxxx\n')
+        for entries in dictionary:
+            the_file.write(entries + "\n")
+
+
 originalCode = readFileLineByLine("original.txt")
 dictionary = createDictionary(originalCode)
 
+# originalCode = readFileLineByLine("sample.txt")
+
 codeEntryIndex = 0
 previousCodeEntry = ''
-compressedCode = []
+# compressedCode = []
+compressedCode = ''
 
 while codeEntryIndex < len(originalCode):
     increment = 1
+    reset_flag = False
     if previousCodeEntry == originalCode[codeEntryIndex]:
         rle_count = RLE(previousCodeEntry, codeEntryIndex, originalCode)
         compressedEntry = '001' + bin(rle_count - 1)[2:].zfill(3)
         increment = rle_count
+        if rle_count == 8:
+            reset_flag = True
     else:
         exactMatchIndex = exactMatch(originalCode[codeEntryIndex])
-        if exactMatchIndex > 0:
+        if exactMatchIndex >= 0:
             compressedEntry = '111' + bin(exactMatchIndex)[2:].zfill(4)
         else:
             isMatched = False
@@ -69,15 +94,15 @@ while codeEntryIndex < len(originalCode):
                 length = len(xorString)
                 xorChunk = getXORedChunk(xorString)
                 if int(xorChunk, 2) == 1:
-                    compressedEntry = '011' + bin(32 - length).zfill(5) + bin(i)[2:].zfill(4)
+                    compressedEntry = '011' + bin(32 - length)[2:].zfill(5) + bin(i)[2:].zfill(4)
                     isMatched = True
                     break
                 elif int(xorChunk, 2) == 3:
-                    compressedEntry = '100' + bin(32 - length).zfill(5) + bin(i)[2:].zfill(4)
+                    compressedEntry = '100' + bin(32 - length)[2:].zfill(5) + bin(i)[2:].zfill(4)
                     isMatched = True
                     break
                 elif int(xorChunk, 2) == 15:
-                    compressedEntry = '101' + bin(32 - length).zfill(5) + bin(i)[2:].zfill(4)
+                    compressedEntry = '101' + bin(32 - length)[2:].zfill(5) + bin(i)[2:].zfill(4)
                     isMatched = True
                     break
                 else:
@@ -89,7 +114,7 @@ while codeEntryIndex < len(originalCode):
                     xorChunk = getXORedChunk(xorString)
                     if len(xorChunk) < 5:
                         xorChunk = xorChunk.ljust(4, '0')
-                        compressedEntry = '010' + bin(32 - length).zfill(5) + xorChunk + bin(i)[2:].zfill(4)
+                        compressedEntry = '010' + bin(32 - length)[2:].zfill(5) + xorChunk + bin(i)[2:].zfill(4)
                         isMatched = True
                         break
                 if not isMatched:
@@ -97,16 +122,20 @@ while codeEntryIndex < len(originalCode):
                         xorString = getXORedString(originalCode[codeEntryIndex], dictionary[i])
                         length = len(xorString)
                         xorChunk = getXORedChunk(xorString)
-                        if xorChunk.count('11')*2 == xorChunk('1') == 4:
+                        if xorChunk.count('1') == 2:
                             firstIndex = 32 - length
-                            secondIndex = firstIndex + xorChunk.rfind('11')
-                            compressedEntry = '010' + bin(firstIndex).zfill(5) + bin(secondIndex).zfill(5) + bin(i)[2:].zfill(4)
+                            secondIndex = firstIndex + xorChunk.rfind('1')
+                            compressedEntry = '110' + bin(firstIndex)[2:].zfill(5) + bin(secondIndex)[2:].zfill(5) + bin(i)[2:].zfill(4)
                             isMatched = True
                             break
                     if not isMatched:
                         compressedEntry = '000' + originalCode[codeEntryIndex]
-    compressedCode.append(compressedEntry)
+
+    # compressedCode.append(compressedEntry)
+    compressedCode = compressedCode + compressedEntry
     previousCodeEntry = originalCode[codeEntryIndex]
+    if reset_flag:
+        previousCodeEntry = ''
     codeEntryIndex = codeEntryIndex + increment
 
-print(compressedCode)
+writeToFile(compressedCode)
